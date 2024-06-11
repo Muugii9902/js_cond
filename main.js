3254bb8f827dacf3ab432834a19dc94ab7178084
Muugii9902 <muugii9902@gmail.com>
2024-06-11 13:10:42 +0800

    js_conditionnals

---
+++ main.js
@@ -0,0 +1,89 @@
+// prompt("ta 1-7 hoorond too songoj oruulan uu !!!");
+
+// let weeknumber = Number(prompt("ta 1-7 hoorond too songoj oruulan uu !!!"));
+// let garigiinNer;
+
+// if (weeknumber === 1) {
+//   console.log("davaa");
+// } else if (weeknumber === 2) {
+//   garigiinNer = "myagmar";
+// } else if (weeknumber === 3) {
+//   garigiinNer = "lhkagvaa";
+// } else if (weeknumber === 4) {
+//   garigiinNer = "purev";
+// } else if (weeknumber === 5) {
+//   garigiinNer = "baasan";
+// } else if (weeknumber === 6) {
+//   garigiinNer = "baymba";
+// } else if (weeknumber === 7) {
+//   garigiinNer = "nyam";
+// } else {
+//   garigiinNer = "oldsonguie";
+// }
+
+// console.log("tanii oruulsan too", weeknumber);
+// console.log("tanii oruulsan toond tohiroh ner", garigiinNer);
+
+// let age = 20;
+// let gender = "er";
+
+// if (age >= 18 && gender === "er") {
+//   console.log("ta nasand hursen eregtei bna");
+// } else {
+//   console.log("ta nasand hureegui esvel emegtei bn");
+// }
+
+// if (age >= 18 || gender === "er") {
+//   console.log("ta nasand hursen esvel eregtei bna");
+// } else {
+//   console.log("ta nasand hureegui esvel emegtei bn");
+// }
+
+// switch (weeknumber) {
+//   case 1: {
+//     garigiinNer = "davaa";
+//     break;
+//   }
+//   case 2: {
+//     garigiinNer = "myagmar";
+//     break;
+//   }
+//   case 3: {
+//     garigiinNer = "lhkagva";
+//     break;
+//   }
+//   case 4: {
+//     garigiinNer = "purev";
+//     break;
+//   }
+//   case 5: {
+//     garigiinNer = "baasan";
+//     break;
+//   }
+//   default: {
+//     garigiinNer = "oldsonguie";
+//   }
+// }
+// console.log("tanii oruulsan too", weeknumber);
+// console.log("tanii oruulsan toond tohiroh ner", garigiinNer);
+
+// let myAge = Number(prompt("ta nasaa oruulna uu"));
+
+// // if (myAge <= 25) {
+// //   console.log("tanii nas 25 doosh bn");
+// // }
+// // if (myAge >= 25) {
+// //   console.log("tanii nas 25 deesh bn");
+// }
+// let n = Number(prompt("ta nasaa oruulna uu"));
+// if (n % 2 === 0) {
+//   console.log("tegshtoo", "event-number");
+// } else if (n % 2 === 1) {
+//   console.log("sondgoi too", "sondgoi");
+// }
+let d = 12,
+  f = 30,
+  h = 45;
+if (d < f && d < h) {
+  console.log("hangiin bga too", d);
+}
